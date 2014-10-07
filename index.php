1c7426b64caaa101dd8a191fc132865d88a39a14
enjoy2000 <enjoy3013@gmail.com>
2014-10-07 16:36:37 +0700

    init index files and git ignore

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Iconic Job Portal</title>
+</head>
+<body align="center">
+    <br />
+    <h1>Iconic Job Portal</h1>
+</body>
+</html>
